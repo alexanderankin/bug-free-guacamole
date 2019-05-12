e20c48f3742d1cc57f8edb9f94c3ae5a76f9b2f3
Dave Ankin <daveankin@gmail.com>
2019-05-12 15:33:28 -0400

    works, need to start splitting

---
+++ src/leaflet-defaulticon-compatibility.js
@@ -0,0 +1,21 @@
+import L from 'leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import icon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import shadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = new L.icon({
+  iconUrl: icon,
+  shadowUrl: shadow,
+  iconRetinaUrl: icon2x,
+
+  // from custom icon docs and https://gis.stackexchange.com/a/223057
+  iconSize:     [24, 41],
+  iconAnchor:   [12, 41],
+
+  // a little to the left - centering
+  // a little more up, match the white dot
+  popupAnchor:  [0, -24]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
---
+++ src/util.js
@@ -0,0 +1,34 @@
+
+
+function isMarkerInsidePolygon(marker, poly) {
+  if (!poly) {
+    console.log("checking isMarkerInsidePolygon empty polygon");
+    return false;
+  }
+
+  if (poly.getLatLngs().length !== 1) {
+    console.log("checking isMarkerInsidePolygon on multiple features");
+    return false;
+  }
+
+  var polyPoints = poly.getLatLngs()[0];
+  var x = marker.getLatLng().lat, y = marker.getLatLng().lng;
+
+  console.log("looking for", x, y, "in polyPoints", polyPoints);
+
+  var inside = false;
+  for (var i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
+    var xi = polyPoints[i].lat, yi = polyPoints[i].lng;
+    var xj = polyPoints[j].lat, yj = polyPoints[j].lng;
+
+    var intersect = ((yi > y) !== (yj > y))
+      && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
+}
+
+module.exports = {
+  isMarkerInsidePolygon
+};
